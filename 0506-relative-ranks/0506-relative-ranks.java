318be5777cca82cdd987295fc343fe1b9781aaf9
Saurav Kalaskar <skalaska@asu.edu>
2025-07-12 23:55:10 -0400

    Time: 7 ms (94.21%), Space: 45.7 MB (63.26%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,30 +1,64 @@
-public class Solution {
-    public String[] findRelativeRanks(int[] nums) {
-        Integer[] index = new Integer[nums.length];
+// public class Solution {
+//     public String[] findRelativeRanks(int[] nums) {
+//         Integer[] index = new Integer[nums.length];
         
-        for (int i = 0; i < nums.length; i++) {
-            index[i] = i;
+//         for (int i = 0; i < nums.length; i++) {
+//             index[i] = i;
+//         }
+        
+//         Arrays.sort(index, (a, b) -> (nums[b] - nums[a]));
+        
+//         String[] result = new String[nums.length];
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (i == 0) {
+//                 result[index[i]] = "Gold Medal";
+//             }
+//             else if (i == 1) {
+//                 result[index[i]] = "Silver Medal";
+//             }
+//             else if (i == 2) {
+//                 result[index[i]] = "Bronze Medal";
+//             }
+//             else {
+//                 result[index[i]] = (i + 1) + "";
+//             }
+//         }
+
+//         return result;
+//     }
+// }
+
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        
+        int n = score.length;
+        String[] res = new String[n];
+        
+        PriorityQueue<Integer> pq = 
+            new PriorityQueue<>((a,b)->score[b]-score[a]);
+        
+        for(int i=0;i<n;i++){
+            pq.add(i);
+        }
+        int i=1;
+        while(!pq.isEmpty()){
+            
+            int idx = pq.poll();
+            
+            if(i>3){
+                res[idx] = Integer.toString(i);
+            }else if(i==1){
+                res[idx] = "Gold Medal";
+            }else if(i==2){
+                res[idx] = "Silver Medal";
+            }else if(i==3){
+                res[idx] = "Bronze Medal";
+            }
+            i++;
         }
         
-        Arrays.sort(index, (a, b) -> (nums[b] - nums[a]));
+        return res;
         
-        String[] result = new String[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                result[index[i]] = "Gold Medal";
-            }
-            else if (i == 1) {
-                result[index[i]] = "Silver Medal";
-            }
-            else if (i == 2) {
-                result[index[i]] = "Bronze Medal";
-            }
-            else {
-                result[index[i]] = (i + 1) + "";
-            }
-        }
-
-        return result;
     }
 }
