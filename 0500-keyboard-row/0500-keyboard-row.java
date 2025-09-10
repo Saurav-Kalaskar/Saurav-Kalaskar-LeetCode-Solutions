af205a9c1d1df624cd074b4e6787d4ac2c1b3c7d
Saurav Kalaskar <skalaska@asu.edu>
2025-09-10 16:33:10 -0700

    Time: 3 ms (3.25%), Space: 41.9 MB (13.28%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,5 +1,5 @@
 class Solution {
     public String[] findWords(String[] words) {
-    return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);        
-    }
+    return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
+}
 }
