31822b905f6cbe82ac8b32839161bf00fe681cc3
Saurav Kalaskar <skalaska@asu.edu>
2025-09-14 21:21:11 -0700

    Time: 4 ms (69.19%), Space: 42.4 MB (8.83%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,17 +1,26 @@
 class Solution {
+    // public int longestPalindrome(String s) {
+    //     if(s==null || s.length() == 0) return 0;
+    //     HashSet<Character> lp = new HashSet<Character>();
+    //     int count = 0;
+    //     for(int i = 0; i<s.length(); i++){
+    //         if(lp.contains(s.charAt(i))){
+    //             lp.remove(s.charAt(i));
+    //             count++;
+    //         }else{
+    //             lp.add(s.charAt(i));
+    //         }
+    //     }
+    //     if (!lp.isEmpty()) return count*2+1;
+    //     return count*2;
+    // }
     public int longestPalindrome(String s) {
-        if(s==null || s.length() == 0) return 0;
-        HashSet<Character> lp = new HashSet<Character>();
-        int count = 0;
-        for(int i = 0; i<s.length(); i++){
-            if(lp.contains(s.charAt(i))){
-                lp.remove(s.charAt(i));
-                count++;
-            }else{
-                lp.add(s.charAt(i));
-            }
+        Set<Character> set = new HashSet<>();
+        int len = 0;
+        for (char c : s.toCharArray()) {
+            if (set.remove(c)) len += 2;
+            else set.add(c);
         }
-        if (!lp.isEmpty()) return count*2+1;
-        return count*2;
+        return set.size() > 0 ? len+1: len;
     }
 }
