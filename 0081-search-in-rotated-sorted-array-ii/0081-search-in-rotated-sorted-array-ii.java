67d478d439f0e4adc1f2ab09fd86f375e87bad1d
Saurav Kalaskar <skalaska@asu.edu>
2025-11-17 18:45:36 -0700

    Time: 0 ms (100%), Space: 45 MB (13.66%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[start] < nums[mid]) {
+                if (nums[start] <= target && target <= nums[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else if (nums[start] > nums[mid]) {
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            } else {
+                start++;
+            }
+        }
+        if (nums[start] == target || nums[end] == target) {
+            return true;
+        }
+        return false;
+    }
+}
