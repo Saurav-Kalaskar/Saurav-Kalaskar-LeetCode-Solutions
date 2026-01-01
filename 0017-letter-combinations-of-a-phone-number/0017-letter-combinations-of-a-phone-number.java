6cbc09dcaecf12fdb6daa63ac7a97eb53b2d9c99
Saurav Kalaskar <skalaska@asu.edu>
2026-01-01 07:25:51 -0700

    Time: 0 ms (100%), Space: 43.6 MB (40.43%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,19 +1,49 @@
+// class Solution {
+//     	public List<String> letterCombinations(String digits) {
+// 		List<String> ans = new LinkedList<String>();
+// 		if (digits.isEmpty())
+// 			return ans;
+// 		String[] mapping = new String[] { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+// 		ans.add(0, "");
+// 		for (int i = 0; i < digits.length(); i++) {
+// 			int x = Character.getNumericValue(digits.charAt(i));
+// 			int size = ans.size();     // number of nodes/strings already in the queue
+// 			for (int k = 1; k <= size; k++) {
+// 				String t = ans.remove(0);
+// 				for (char s : mapping[x].toCharArray())
+// 					ans.add(t + s);
+// 			}
+// 		}
+// 		return ans;
+// 	}
+// }
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    	public List<String> letterCombinations(String digits) {
-		List<String> ans = new LinkedList<String>();
-		if (digits.isEmpty())
-			return ans;
-		String[] mapping = new String[] { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-		ans.add(0, "");
-		for (int i = 0; i < digits.length(); i++) {
-			int x = Character.getNumericValue(digits.charAt(i));
-			int size = ans.size();     // number of nodes/strings already in the queue
-			for (int k = 1; k <= size; k++) {
-				String t = ans.remove(0);
-				for (char s : mapping[x].toCharArray())
-					ans.add(t + s);
-			}
-		}
-		return ans;
-	}
+    private static final String[] PHONE_MAP = {
+        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+    
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<>();
+        if (digits.isEmpty()) return combinations;
+        backtrack(0, digits, new StringBuilder(), combinations);
+        return combinations;
+    }
+    
+    private void backtrack(int index, String digits, StringBuilder path, List<String> combinations) {
+        if (index == digits.length()) {
+            combinations.add(path.toString());
+            return;
+        }
+        
+        String possibleLetters = PHONE_MAP[digits.charAt(index) - '0'];
+        for (char letter : possibleLetters.toCharArray()) {
+            path.append(letter);
+            backtrack(index + 1, digits, path, combinations);
+            path.deleteCharAt(path.length() - 1); // Backtrack
+        }
+    }
 }
