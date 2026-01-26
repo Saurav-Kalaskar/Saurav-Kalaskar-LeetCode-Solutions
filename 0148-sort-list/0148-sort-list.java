a0dec7466a181a899742a002f2c3b285b92ca256
Saurav Kalaskar <skalaska@asu.edu>
2026-01-25 18:16:50 -0700

    Time: 9 ms (98.49%), Space: 59 MB (96.52%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,64 +9,51 @@
  * }
  */
 class Solution {
-    public ListNode sortList(ListNode head) {
-  ListNode dummy = new ListNode(0);
-  dummy.next = head;
-
-  ListNode [] sublists = new ListNode[2];
-  ListNode [] sublists_tail = new ListNode[2];
-
-  // Grab sublists of size 1, then 2, then 4, etc, until fully merged
-  for (int steps = 1;; steps *= 2) {
-    // Record the progress of the current pass into a single semi sorted list by updating
-    // the next of the previous node (or the dummy on the first loop)
-    ListNode prev = dummy;
-
-    // Keep track of how much is left to process on this pass of the list
-    ListNode remaining = prev.next;
-
-    int num_loops = 0;
-    for (; null != remaining; ++num_loops) {
-      // Split 2 sublists of steps length from the front
-      for (int i = 0; i < 2; ++i) {
-        sublists[i] = remaining;
-        sublists_tail[i] = null;
-        for (int j = 0; null != remaining && j < steps; ++j) {
-          sublists_tail[i] = remaining;
-          remaining = remaining.next;
-        }
-        // Ensure the subslist (if one was made) is terminated
-        if (null != sublists_tail[i]) {
-          sublists_tail[i].next = null;
-        }
-      }
-
-      // We have two sublists of (upto) length step that are sorted, merge them onto the end into a single list of (upto) step * 2
-      while (null != sublists[0] && null != sublists[1]) {
-        if (sublists[0].val <= sublists[1].val) {
-          prev.next = sublists[0];
-          sublists[0] = sublists[0].next;
-        } else {
-          prev.next = sublists[1];
-          sublists[1] = sublists[1].next;
-        }
-        prev = prev.next;
-      }   
-
-      // One list has been finished, attach what ever is left of the other to the end
-      if (null != sublists[0]) {
-        prev.next = sublists[0];
-        prev = sublists_tail[0];
-      } else {
-        prev.next = sublists[1];
-        prev = sublists_tail[1];
-      }
+  
+  public ListNode sortList(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+        
+    // step 1. cut the list to two halves
+    ListNode prev = null, slow = head, fast = head;
+    
+    while (fast != null && fast.next != null) {
+      prev = slow;
+      slow = slow.next;
+      fast = fast.next.next;
     }
-
-    // If the entire list was fully processed in a single loop, it means we've completely sorted the list and are done
-    if (1 >= num_loops) {
-      return dummy.next;
-    }
+    
+    prev.next = null;
+    
+    // step 2. sort each half
+    ListNode l1 = sortList(head);
+    ListNode l2 = sortList(slow);
+    
+    // step 3. merge l1 and l2
+    return merge(l1, l2);
   }
-}
+  
+  ListNode merge(ListNode l1, ListNode l2) {
+    ListNode l = new ListNode(0), p = l;
+    
+    while (l1 != null && l2 != null) {
+      if (l1.val < l2.val) {
+        p.next = l1;
+        l1 = l1.next;
+      } else {
+        p.next = l2;
+        l2 = l2.next;
+      }
+      p = p.next;
+    }
+    
+    if (l1 != null)
+      p.next = l1;
+    
+    if (l2 != null)
+      p.next = l2;
+    
+    return l.next;
+  }
+
 }
