3769b7ccd80f63ff1502f8f9d63b15dcbc179cd1
Saurav Kalaskar <skalaska@asu.edu>
2026-03-08 23:52:49 -0700

    Time: 309 ms (94.75%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
-SELECT ROW_NUMBER() OVER() id, student
-FROM seat
-ORDER BY IF(MOD(id, 2) = 0, id-1, id+1)
+SELECT
+  CASE
+    WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
+    WHEN id % 2 = 1 THEN id + 1
+    ELSE id - 1
+  END AS id,
+  student
+FROM Seat
+ORDER BY id;
