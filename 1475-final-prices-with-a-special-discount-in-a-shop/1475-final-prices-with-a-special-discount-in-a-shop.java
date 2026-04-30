c64f07462461f0d142ba50147f0b90740bec74b4
Saurav Kalaskar <skalaska@asu.edu>
2026-04-29 20:57:01 -0700

    Time: 4 ms (54.68%), Space: 45 MB (86.57%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] result = prices.clone();
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[i] <= prices[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = prices[index] - prices[i];
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
